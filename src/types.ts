bc38809ffe12f01c8eaaeb54b91246a5f9b3711b
randilu <randilu.soysa@gmail.com>
2024-10-04 11:55:54 +0300

    Add types

---
+++ src/types.ts
@@ -0,0 +1,40 @@
+export interface User {
+  id: number;
+  name: string;
+  email: string;
+  phone: string;
+  website: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+  };
+}
+
+
+
+
+export interface UserResponse {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lat: string;
+      lng: string;
+    };
+  };
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
+}
