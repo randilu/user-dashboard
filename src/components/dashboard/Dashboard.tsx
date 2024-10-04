7a25c22e630fde5a957e1479723c1144941bd4fe
randilu <randilu.soysa@gmail.com>
2024-10-04 10:29:25 +0300

    Add components

---
+++ src/components/dashboard/Dashboard.tsx
@@ -0,0 +1,14 @@
+
+
+import styles from "./styles.module.scss";
+
+function Dashboard() {
+   
+  return (
+    <div className={styles.dashboardContainer}>
+      <h1>Dashboard</h1>
+    </div>
+  );
+}
+
+export default Dashboard;
